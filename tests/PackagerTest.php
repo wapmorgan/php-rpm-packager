ce65a0866088c6b0f69e716ad0f9c0b0a31cec5d
wapmorgan <wapmorgan@gmail.com>
2015-05-25 23:59:29 +0300

    Add complex Packager test

--- tests/PackagerTest.php
+++ tests/PackagerTest.php
@@ -32,6 +32,7 @@ class PackagerTest extends PHPUnit_Framework_TestCase {
         $packager->setOutputPath(__DIR__.'/output');
         $packager->addMount($dir, '/');
         $packager->run();
+
         $this->assertEquals('%autosetup -c package', $spec->prep);
         $this->assertEquals('rm -rf %{buildroot}'."\n".'mkdir -p %{buildroot}'."\n".'cp -rp * %{buildroot}', $spec->install);
         $this->assertEquals('/*', $spec->files);
@@ -41,6 +42,59 @@ class PackagerTest extends PHPUnit_Framework_TestCase {
         $phar = new PharData($_SERVER['HOME'].'/rpmbuild/SOURCES/test.tar');
         $this->assertTrue(isset($phar['test/binary']));
         $this->assertTrue(isset($phar['test2/abc']));
+
+        $command = $packager->build();
+        $this->assertEquals('rpmbuild -bb '.$_SERVER['HOME'].'/rpmbuild/SPECS/test.spec', $command);
+        exec($command, $_, $result);
+        $this->assertEquals(0, $result);
+        $this->assertTrue(file_exists($_SERVER['HOME'].'/rpmbuild/RPMS/noarch/test-0.1-1.noarch.rpm'));
+
+        unlink($_SERVER['HOME'].'/rpmbuild/RPMS/noarch/test-0.1-1.noarch.rpm');
+        unlink($_SERVER['HOME'].'/rpmbuild/SPECS/test.spec');
+        unlink($_SERVER['HOME'].'/rpmbuild/SOURCES/test.tar');
+
+        $this->removeDir($dir);
+        $this->removeDir(__DIR__.'/output');
+    }
+
+    public function testComplex() {
+        exec('command -v rpm', $output, $result);
+        if (empty($output)) {
+            $this->markTestSkipped('This test can not be performed on a system without rpm');
+        }
+
+        $dir = __DIR__.'/package/';
+        if (is_dir($dir))
+            $this->removeDir($dir);
+
+        mkdir($dir);
+        mkdir($dir.'/test', 0755, true);
+        touch($dir.'/test/binary');
+        mkdir($dir.'/test2');
+        touch($dir.'/test2/abc');
+
+        $spec = new Spec();
+        $spec->setPackageName('test');
+        $packager = new Packager();
+        $packager->setSpec($spec);
+
+        if (is_dir(__DIR__.'/output'))
+            $this->removeDir(__DIR__.'/output');
+
+        $packager->setOutputPath(__DIR__.'/output');
+        $packager->addMount($dir.'/test/binary', '/usr/bin/binary');
+        $packager->addMount($dir.'/test2/', '/usr/lib/test/');
+        $packager->run();
+
+        $this->assertEquals('%autosetup -c package', $spec->prep);
+        $this->assertEquals('rm -rf %{buildroot}'."\n".'mkdir -p %{buildroot}'."\n".'cp -rp * %{buildroot}', $spec->install);
+        $this->assertEquals('/*', $spec->files);
+        $this->assertTrue(file_exists($_SERVER['HOME'].'/rpmbuild/SPECS/test.spec'));
+        $this->assertTrue(file_exists($_SERVER['HOME'].'/rpmbuild/SOURCES/test.tar'));
+
+        $phar = new PharData($_SERVER['HOME'].'/rpmbuild/SOURCES/test.tar');
+        $this->assertTrue(isset($phar['usr/bin/binary']));
+        $this->assertTrue(isset($phar['usr/lib/test/abc']));
 
         $command = $packager->build();
         $this->assertEquals('rpmbuild -bb '.$_SERVER['HOME'].'/rpmbuild/SPECS/test.spec', $command);
